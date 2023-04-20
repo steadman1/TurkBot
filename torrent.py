bc3f4741a1c31e323b4fb0cb67a3f76b9dffcf9c
Spencer <64872573+steadman1@users.noreply.github.com>
2023-04-19 23:36:27 -0400

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,7 +1,37 @@
 from qbittorrent import Client
 import os
 
-class QBittorrent:
+class PyTorrent:
+    def __init__(self, category, save_path="F:\Movies"):
+        try:
+            self.qb = Client('http://127.0.0.1:8080/')
+            self.qb.login('admin', 'root420')
+        except ConnectionError:
+            print("Please open the qBittorrent App and start qbittorrent server in the Tools > Options > Web UI Interface (remote control).")
+        self.category = category
+        self.save_path = save_path # os.path.join(os.path.dirname(__file__), "movies")
+
+    def download(self, torrent_id):
+        """
+        Download a torrent file
+        
+        params:
+        torrent_id can be:
+            * magnet uri
+            * http url to .torrent file
+            * filesystem path to .torrent file
+            * info hash (hex string)
+        """
+        self.qb.download_from_link(torrent_id, savepath=self.save_path, category=self.category)
+
+    def cancel(self, torrent_id):
+        """Cancel a torrent download and delete all related data"""
+        self.qb.delete_permanently(torrent_id)
+
+    def status(self, results_count=5):
+        return [QBittorrent(**torrent) for torrent in self.qb.torrents(category=self.category)[:results_count]]
+    
+    class QBittorrent:
     def __init__(self, added_on, amount_left, auto_tmm, availability, category, completed, completion_on, content_path, dl_limit, dlspeed, download_path, downloaded, downloaded_session, eta, f_l_piece_prio, force_start, hash, infohash_v1, infohash_v2, last_activity, magnet_uri, max_ratio, max_seeding_time, name, num_complete, num_incomplete, num_leechs, num_seeds, priority, progress, ratio, ratio_limit, save_path, seeding_time, seeding_time_limit, seen_complete, seq_dl, size, state, super_seeding, tags, time_active, total_size, tracker, trackers_count, up_limit, uploaded, uploaded_session, upspeed):
         self.added_on = added_on
         self.amount_left = amount_left
@@ -52,33 +82,3 @@ class QBittorrent:
         self.uploaded = uploaded
         self.uploaded_session = uploaded_session
         self.upspeed = upspeed
-
-class PyTorrent:
-    def __init__(self, category):
-        try:
-            self.qb = Client('http://127.0.0.1:8080/')
-            self.qb.login('admin', 'root420')
-        except ConnectionError:
-            print("Please open the qBittorrent App and start qbittorrent server in the Tools > Options > Web UI Interface (remote control).")
-        self.category = category
-
-    def download(self, torrent_id):
-        """
-        Download a torrent file
-        
-        params:
-        torrent_id can be:
-            * magnet uri
-            * http url to .torrent file
-            * filesystem path to .torrent file
-            * info hash (hex string)
-        """
-        save_path = "F:\Movies" # os.path.join(os.path.dirname(__file__), "movies")
-        self.qb.download_from_link(torrent_id, savepath=save_path, category=self.category)
-
-    def cancel(self, torrent_id):
-        """Cancel a torrent download and delete all related data"""
-        self.qb.delete_permanently(torrent_id)
-
-    def status(self, results_count=5):
-        return [QBittorrent(**torrent) for torrent in self.qb.torrents(category=self.category)[:results_count]]
